af269b3dfa12781af717fa299a58e6eface18db3
Keller Scholl <keller.scholl@gmail.com>
2015-03-11 21:43:55 +0000

    Added basic project outline.

---
+++ autoChange.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#In theory, this program should automatically change my mac address
+#once I get it working.
+echo "Hello World"
+#Step 1: Get some random data.
+#Step 2: Create a new MAC address.
+#Step 3: Create a new hardware address.
